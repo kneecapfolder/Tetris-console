9bb8d6395ae8f2c8908bde1e13d827ff9c366f38
kneecapFolder <ilan7gr@gmail.com>
2024-07-20 22:05:03 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Tetris {
+    class Program {
+        static ConsoleKey key;
+
+        static void Main(string[] args) {
+            // Setup external terminal
+            Console.Title = "Tetris";
+            Console.SetWindowSize(20, 20);
+            Console.SetBufferSize(20, 20);
+
+            while(key != ConsoleKey.Escape) {
+                Thread inputThread = new Thread(input);
+                Console.CursorVisible = false;
+                inputThread.Start();
+            }
+        }
+
+        static void input() {
+            if (Console.KeyAvailable)
+                key = Console.ReadKey().Key;
+        }
+    }
+}
